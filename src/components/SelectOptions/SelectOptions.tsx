5011d1e6656df4b87b78c08317dad7df72336a93
Gian Manzo <ggianmanzo@gmail.com>
2022-08-07 19:03:20 -0300

    custom page 2

---
+++ src/components/SelectOptions/SelectOptions.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import { IOptions } from '~/interfaces/IOptions';
+import styles from './styles.module.scss';
+export const SelectOptions = ({ title, description, icon, selected, onClick }: IOptions) => {
+  return (
+    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+    <div
+      onClick={onClick}
+      className={styles.container}
+      style={{ borderColor: selected ? '#25cd89' : '#16195c' }}
+    >
+      <div className={styles.icon}>{icon}</div>
+      <div className={styles.info}>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.description}>{description}</div>
+      </div>
+    </div>
+  );
+};
