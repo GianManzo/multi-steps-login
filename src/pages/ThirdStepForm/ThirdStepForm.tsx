069fbec8a623f7a7d7e1a399abdc7f5a3a9f836f
Gian Manzo <ggianmanzo@gmail.com>
2022-08-08 18:55:07 -0300

    add third page and custom

--- src/pages/ThirdStepForm/ThirdStepForm.tsx
+++ src/pages/ThirdStepForm/ThirdStepForm.tsx
@@ -1,5 +1,74 @@
+import { ChangeEvent, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { info } from 'sass';
+import { Theme } from '~/components/Theme/Theme';
+import { useForm } from '~/contexts/FormContext';
+import { FormActions } from '~/enum/enum';
 import styles from './styles.module.scss';
 
+FormActions;
 export const ThirdStepForm = () => {
-  return <div className={styles.container}>Passo 3</div>;
+  const navigate = useNavigate();
+  const { state, dispatch } = useForm();
+
+  const errorMessage = () => {
+    const error = document.querySelector('span');
+    error!.innerText = 'Preencha o campo para prosseguir!';
+    setTimeout(() => {
+      error!.innerText = '';
+    }, 3000);
+  };
+
+  const handleNextStep = () => {
+    if (state.email !== '' && state.github !== '') console.log(state);
+    else alert('preencha os dados');
+  };
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: FormActions.setEmail,
+      payload: e.target.value,
+    });
+  };
+
+  const handleGitChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: FormActions.setGithub,
+      payload: e.target.value,
+    });
+  };
+
+  useEffect(() => {
+    if (!state.name) navigate('/');
+    dispatch({
+      type: FormActions.setCurrentStep,
+      payload: 3,
+    });
+  }, [dispatch, navigate, state.name]);
+
+  return (
+    <Theme>
+      <div className={styles.container}>
+        <p>Passo 3/3</p>
+        <h1>Onde podemos te encontrar {state.name}?</h1>
+        <p>Preencha suas informações de contato abaixo!.</p>
+
+        <hr />
+
+        <label>
+          Seu email
+          <input type='email' value={state.email} onChange={handleEmailChange} />
+          <span className={styles.error}></span>
+        </label>
+        <label>
+          Seu GitHub
+          <input type='url' value={state.github} onChange={handleGitChange} />
+        </label>
+
+        <Link to='/step2'>Voltar</Link>
+        <button onClick={handleNextStep}>Finalizar cadastro</button>
+      </div>
+    </Theme>
+  );
 };
